83ff866990a1c51a41491b75ba50d27dcd01eb8b
Kevin Dutzy <kddutz15@gmail.com>
2022-11-05 15:51:02 -0400

    commit tree 2494777783cc9bb33f790c033d6bf32b8e9ce970

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    const yearWon = record.find(record => record.result === 'W')
+    if (yearWon) {
+        return yearWon.year
+    } else {return undefined}
+}
+
+
